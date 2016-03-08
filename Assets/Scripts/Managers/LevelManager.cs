456fe1955ca9f64152093d035560907877c74b63
teachmehowtoprogram <corvushospes@gmail.com>
2016-03-08 12:00:34 +0300

    Восстановление хп при лвлапе
    Пока что отображается только после получения дмга

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -2,36 +2,45 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
 
-	public static int currentExp = 0;
-	public static int level = 1;
+    public static int currentExp = 0;
+    public static int level = 1;
+    static GameObject player;
+    static PlayerHealth playerHealth;
+
     static Slider expSlider;
     static Text levelText;
-	static AudioSource lvlupAudio;
+    static AudioSource lvlupAudio;
 
-	public static void TakeExp(int count)
-	{
-		currentExp += count;
+    public static void TakeExp(int count)
+    {
+        currentExp += count;
         if (currentExp >= level * 100)
         {
             currentExp = currentExp - level * 100;
             level += 1;
+            playerHealth.currentHealth = playerHealth.startingHealth;
             lvlupAudio.Play();
         }
     }
 
 
-    void Start ()
-	{
-		lvlupAudio = gameObject.GetComponent<AudioSource> ();
-		expSlider = transform.GetChild(0).GetComponent<Slider>();
-		levelText = transform.GetChild(1).GetComponent<Text>();
-	    expSlider.value = 0;
+    void Start()
+    {
+        lvlupAudio = gameObject.GetComponent<AudioSource>();
+        expSlider = transform.GetChild(0).GetComponent<Slider>();
+        levelText = transform.GetChild(1).GetComponent<Text>();
+        expSlider.value = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
 
-	}
 
-	void Update () {
+    }
+
+    void Update()
+    {
         expSlider.maxValue = level * 100;
         expSlider.value = currentExp;
         levelText.text = level.ToString();
