1dfd7db88ef27df87696555c6cf1e41ba6963416
teachmehowtoprogram <corvushospes@gmail.com>
2016-03-27 15:08:18 +0300

    Сделал волны
    бадам - тсс

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -11,18 +12,54 @@ public class EnemyManager : MonoBehaviour
     void Start ()
     {
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        StartCoroutine (Spawn ());
     }
 
 
-    void Spawn ()
+   IEnumerator Spawn ()
     {
-        if(playerHealth.currentHealth <= 0f)
+     yield return new WaitForSeconds (15); // время до первой волны в секуднах
+        while (true)
+        {
+            if (playerHealth.currentHealth <= 0f)
+            {
+                yield break;
+            }
+
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            yield return new WaitForSeconds(10); // время между волнами в секундах
+        }
+    }
+}
+
+
+/*using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints; //шта? массив спавн поинтов?
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        if (playerHealth.currentHealth <= 0f)
         {
             return;
         }
 
-        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
-        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
-}
+}*/
