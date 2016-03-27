c21a78800f88720222f9459de7d19dc9aa0dc296
5000factorial <tema968@gmail.com>
2016-03-28 00:14:55 +0300

    Починил квесты. Все работает(вроде). Начал делать нотификейшены.

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 using System.Collections;
 using System.IO;
 using System.Threading;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SaveManager : MonoBehaviour
 {
     Text text;
-
-    // Use this for initialization
+    
     private void Start()
     {
         text = GetComponent<Text>();
     }
-
-    // Update is called once per frame
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F5))
@@ -39,16 +38,11 @@ public class SaveManager : MonoBehaviour
 
     public void LoadGame()
     {
-        Application.LoadLevel(1);
+        SceneManager.LoadScene(1);
         FileStream file1 = new FileStream("Save.txt", FileMode.Open); //создаем файловый поток
         StreamReader reader = new StreamReader(file1); // создаем «потоковый читатель» и связываем его с файловым потоком 
         GoldManager.gold = int.Parse(reader.ReadLine());
         LevelManager.level = int.Parse(reader.ReadLine());
         LevelManager.currentExp = int.Parse(reader.ReadLine());
-    }
-
-    public void DeleteSave()
-    {
-
     }
 }
